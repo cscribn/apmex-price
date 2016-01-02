4f04fb4dc645c938081b8fabe8d991f85fb628b4
Chad Scribner <chad.scribner@firespring.com>
2016-01-02 14:39:12 -0600

    Updating test URLs

--- test/test-apmex-price.rb
+++ test/test-apmex-price.rb
@@ -9,17 +9,17 @@ class ApmexPriceTest < Minitest::Test
 			'http://www.apmex.com/product/23331/1-oz-silver-american-eagle-random-year',
 			'http://www.apmex.com/product/23331/1-oz-silver-american-eagle-random-year',
 			'http://www.apmex.com/product/23331/1-oz-silver-american-eagle-random-year',
-			'http://www.apmex.com/product/79019/2014-1-oz-silver-american-eagle-bu',
+			'http://www.apmex.com/product/23331/1-oz-silver-american-eagle-random-year',
 			'http://www.apmex.com/product/54875/2010-australia-1-oz-silver-kookaburra-bu',
 			'http://www.apmex.com/product/54875/2010-australia-1-oz-silver-kookaburra-bu',
 			'http://www.apmex.com/product/84445/2015-australia-1-oz-silver-kookaburra-bu',
-			'http://www.apmex.com/product/79047/2014-1-10-oz-gold-austrian-philharmonic',
-			'http://www.apmex.com/product/79021/2014-austria-1-oz-silver-philharmonic',
+			'http://www.apmex.com/product/4233/austria-1-10-oz-gold-philharmonic-bu-random-year',
+			'http://www.apmex.com/product/55369/austria-1-oz-silver-philharmonic-random-year',
 			'http://www.apmex.com/product/79573/2014-1-oz-silver-britannia-brilliant-uncirculated',
 			'http://www.apmex.com/product/12/1-10-oz-gold-canadian-maple-leaf-random-year',
-			'http://www.apmex.com/product/79041/2014-canada-1-4-oz-gold-maple-leaf-bu',
+			'http://www.apmex.com/product/11/canada-1-4-oz-gold-maple-leaf-random-year',
 			'http://www.apmex.com/product/1090/1-oz-silver-canadian-maple-leaf-random-year',
-			'http://www.apmex.com/product/72454/2013-china-1-10-oz-gold-panda-bu-sealed',
+			'http://www.apmex.com/product/26849/china-1-10-oz-gold-panda-bu-random-year-sealed-in-plastic',
 			'http://www.apmex.com/product/72458/2013-china-1-oz-silver-panda-bu-in-capsule',
 			'http://www.apmex.com/product/1114/1-oz-silver-mexican-libertad-random-year',
 			'http://www.apmex.com/product/11951/1-oz-pamp-suisse-gold-bar-9999-fine-in-assay',
@@ -28,9 +28,9 @@ class ApmexPriceTest < Minitest::Test
 			'http://www.apmex.com/product/72281/10-oz-sunshine-silver-bar-999-fine-v2',
 			'http://www.apmex.com/product/72281/10-oz-sunshine-silver-bar-999-fine-v2'
 		]
-		
+
 		puts 'Value,Spot,Buy,Sell'
-		
+
 		apmex_urls.each do |u|
 			a = ApmexPrice.new(u)
 			puts a.to_s
@@ -38,6 +38,6 @@ class ApmexPriceTest < Minitest::Test
 			assert a.ounces_spot.is_a? Float
 			assert a.buy.is_a? Float
 			assert a.sell.is_a? Float
-		end	
+		end
 	end
 end
